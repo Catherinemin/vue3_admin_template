4d7cf0c6a1ed8afbdf015e33ce625ba114614a18
Catherinemin <1115320273@qq.com>
2024-08-30 15:09:52 +0800

    Add new feature

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>vue app</h1>
+  </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main></main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
